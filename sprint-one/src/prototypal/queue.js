742821c98e42b983e05c101f509037257cf731eb
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-01-17 15:57:30 -0800

    Completes queue and stack for prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.count = Object.keys(someInstance.storage).length;
+
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  dequeue: function() {
+    var item;
+    if (this.count === 0) {
+      return;
+    } else {
+      item = this.storage[0];
+      delete this.storage[0];
+      this.count --;
+      for (var key in this.storage) {
+        this.storage[Number.parseInt(key, 10) - 1] = this.storage[key];
+      }
+    }
+
+    return item;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.count = Object.keys(someInstance.storage).length;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    var item;
+    if (this.count === 0) {
+      return;
+    } else {
+      item = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count--;
+    }
+    return item;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
