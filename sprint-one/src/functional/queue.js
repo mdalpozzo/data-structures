592410f384f9eb7cb59e078d8a052433f3f778a5
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-01-17 11:46:54 -0800

    Satisfies test requirements for queues in the functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,32 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = Object.keys(someInstance).length;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var item;
+    if (count === 0) {
+      return;
+    } else {
+      item = someInstance[count - 1];
+      delete someInstance[count - 1];
+      count--;
+      for (var key in someInstance) {
+        key = Number.parseInt(key, 10) - 1
+      }
+    }
+    return item;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
