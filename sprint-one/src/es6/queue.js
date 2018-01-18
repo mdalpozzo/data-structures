82fbf8a6e614f5db92b391d4638f38fcc4c6b0c8
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-01-17 16:41:17 -0800

    Completes es6 pattern for stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,34 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = Object.keys(this.storage).length;
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var item;
+    if (this.count === 0) {
+      return;
+    } else {
+      item = this.storage[0];
+      delete this.storage[0];
+      this.count--;
+      for (var key in this.storage) {
+        this.storage[Number.parseInt(key, 10) - 1] = this.storage[key];
+      }
+    }
+    
+    return item;
+  }
+
+  size() {
+    return this.count;
+  }
 }
+
+var queue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = Object.keys(this.storage).length;
+  }
+
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    var item;
+    if (this.count === 0) {
+      return;
+    } else {
+      item = this.storage[this.count - 1];
+      delete this.storage[this.count - 1];
+      this.count--;
+    }
+
+    return item;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
+
+var stack = new Stack();
